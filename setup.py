148d7c8bd68f2397c57b76ea4c6b08ad18e307e4
Johannes Weskamm <weskamm@terrestris.de>
2026-07-16 16:27:42 +0200

    Upgrade workflows to use python 3.14

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Internet :: Proxy Servers",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
         "Topic :: Scientific/Engineering :: GIS",
